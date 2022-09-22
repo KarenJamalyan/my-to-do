a614089ca3b8649f5aca4e8c1ef3728d5a7e0158
unknown <you@example.com>
2022-09-22 17:51:53 +0400

    error in reducer

--- src/components/TaskListItem/TaskListItem.tsx
+++ src/components/TaskListItem/TaskListItem.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { FaTrashAlt, FaPencilAlt, FaRegCheckCircle, FaRegCircle } from 'react-icons/fa';
 import * as s from './style';
 import { ITask } from '../../models/models';
+import { useAppDispatch } from '../../hooks/redux';
+import { todoSlice } from '../../store/reducers/todoSilce';
 
 
 const TaskListItem: React.FC<{task:ITask, todoId: string}> = ({task, todoId}) => {
+
+    const dispatch = useAppDispatch()
+    const { deleteTask } = todoSlice.actions
+
     return(
         <>
             <p>{task.txt}</p>
             <s.Buttons>
                 <span>
-                    <FaTrashAlt />  
+                    <FaPencilAlt />
                 </span>
                 <span>
-                    <FaPencilAlt /> 
+                    <FaTrashAlt  onClick={() => {dispatch(deleteTask({ taskId: task.id, todoId: todoId }))}}    /> 
                 </span>
             </s.Buttons>
         </>
--- src/store/reducers/todoSilce.ts
+++ src/store/reducers/todoSilce.ts
@@ -1,13 +1,13 @@
-import {TodoState, IToDo} from "../../models/models"
+import {TodoState, IToDo, ITask} from "../../models/models"
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 
 const initialState: TodoState = {
     todos: [
-        { id:'000001', title:" Tasak mek 1", tasks: [{ id: '5454', txt: 'havaqel todu', status: false}] },
-        { id:'000002', title:" Tasak mek 2", tasks: [{ id: '5252', txt: 'havaqel todu', status: false}] },
-        { id:'000003', title:" Tasak mek 1", tasks: [{ id: '5456', txt: 'havaqel todu', status: false}, { id: '58456', txt: 'havaqel todu', status: false}] },
-        { id:'000004', title:" Tasak mek 2", tasks: [{ id: '5259', txt: 'havaqel todu', status: false}] },
+        { id:'000001', title: 'Tasak mek 1', tasks: [{ id: '5454', txt: 'havaqel todu', status: false}] },
+        { id:'000002', title: 'Tasak mek 2', tasks: [{ id: '5252', txt: 'havaqel todu', status: false}] },
+        { id:'000003', title: 'Tasak mek 1', tasks: [{ id: '5456', txt: 'havaqel todu', status: false}, { id: '58456', txt: 'havaqel todu', status: false}] },
+        { id:'000004', title: 'Tasak mek 2', tasks: [{ id: '5259', txt: 'havaqel todu', status: false}] },
     ],
     error: null,
     editId: null,
@@ -29,6 +29,20 @@ export const todoSlice = createSlice({
         },
         changingId(state, action: PayloadAction<string | null>){
             state.editId = action.payload
+        },
+        deleteTask(state, action: PayloadAction<{taskId:string, todoId:string}>){
+            let a:any = state.todos.map((elem:IToDo) => {
+                    if(elem.id === action.payload.todoId){
+                        let tasks:ITask[]  = elem.tasks.filter((item:ITask ) => item.id !== action.payload.taskId
+                         return {
+                            ...elem,
+                            elem.tasks: tasks
+                        }
+                    }else{
+                        return elem
+                    }
+            })
+            console.log(a);
         }
     }
 })
